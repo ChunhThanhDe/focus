b3b14e1c4724c11b8f38a74925005e1ff8dda562
ChunhThanhDe <chunhthanhde.dev@gmail.com>
2025-05-27 16:52:42 +0700

    chore: update pubspec.yaml with new dependencies and font assets
    - Added Flutter SDK constraint for version >=3.22.0 <4.0.0
    - Included new dependencies: unsplash_client, shared, equatable, shared_preferences, provider, get_it, json_annotation, collection, intl, http, file_picker, screwdriver, flutter_multi_formatter, url_launcher, package_info_plus, flutter_screwdriver, flutter_markdown, mobx, flutter_mobx, rxdart, universal_html, universal_io
    - Added dev_dependencies: build_runner, characters, dart_style, json_serializable, flutter_launcher_icons, yaml, mobx_codegen
    - Updated assets section to include fonts and images directories
    - Defined custom font families and their respective font assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,148 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
-void main() {
+import 'backend/backend_service.dart';
+import 'backend/rest_backend_service.dart';
+import 'home/home.dart';
+import 'resources/colors.dart';
+import 'utils/geocoding_service.dart';
+import 'utils/storage_manager.dart';
+import 'utils/weather_service.dart';
+
+void main() async {
+  final binding = WidgetsFlutterBinding.ensureInitialized();
+  for (var view in binding.renderViews) {
+    view.automaticSystemUiAdjustment = false;
+  }
+  await initialize();
   runApp(const MyApp());
 }
+
+late PackageInfo packageInfo;
+
+Future<void> loadPackageInfo() async => packageInfo = await PackageInfo.fromPlatform();
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return DebugRender(
+      debugHighlightObserverRebuild: false,
+      child: MaterialApp(
+        title: 'Pluto',
+        debugShowCheckedModeBanner: false,
+        theme: buildTheme(context),
+        home: const HomeWrapper(
+          key: ValueKey('HomeWrapper'),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+Future<void> initialize() async {
+  await initializeBackend();
+
+  final storage = await SharedPreferencesStorageManager.create();
+  GetIt.instance.registerSingleton<LocalStorageManager>(storage);
+  GetIt.instance.registerSingleton<WeatherService>(OpenMeteoWeatherService());
+  GetIt.instance.registerSingleton<GeocodingService>(OpenMeteoGeocodingService());
+
+  await GetIt.instance.allReady();
+  await loadPackageInfo();
+}
+
+Future<void> initializeBackend() async {
+  // Initialize Celest
+  final BackendService service = await getBackend();
+
+  await service.init(local: useLocalServer);
+
+  GetIt.instance.registerSingleton<BackendService>(service);
+}
+
+Future<BackendService> getBackend() async => RestBackendService();
+
+ThemeData buildTheme(BuildContext context) {
+  return ThemeData(
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: CupertinoColors.systemBlue,
+      brightness: Brightness.dark,
+      primary: CupertinoColors.systemBlue,
+    ),
+    useMaterial3: true,
+    scaffoldBackgroundColor: Colors.black,
+    // canvasColor: Colors.black,
+    // cardColor: Colors.black,
+    brightness: Brightness.dark,
+    dividerColor: AppColors.borderColor,
+    scrollbarTheme: ScrollbarThemeData(
+      thickness: WidgetStateProperty.all(4),
+      thumbVisibility: WidgetStateProperty.all(true),
+    ),
+    // fontFamily: FontFamilies.product,
+    tooltipTheme: TooltipThemeData(
+      waitDuration: const Duration(milliseconds: 300),
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+      verticalOffset: 18,
+      textStyle: const TextStyle(
+        fontSize: 12,
+        color: Colors.white60,
+      ),
+      decoration: ShapeDecoration(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(4),
+        ),
+        color: Colors.grey.shade900,
+      ),
+    ),
+    textTheme: const TextTheme(
+      displayLarge: TextStyle(
+        color: AppColors.textColor,
+      ),
+      displayMedium: TextStyle(
+        color: AppColors.textColor,
+      ),
+      displaySmall: TextStyle(
+        color: AppColors.textColor,
+      ),
+      headlineMedium: TextStyle(
+        color: AppColors.textColor,
+      ),
+      headlineSmall: TextStyle(
+        color: AppColors.textColor,
+      ),
+      titleLarge: TextStyle(
+        color: AppColors.textColor,
+      ),
+      bodyLarge: TextStyle(
+        color: AppColors.textColor,
+      ),
+      bodyMedium: TextStyle(
+        color: AppColors.textColor,
+      ),
+    ),
+  );
+}
+
+class DebugRender extends InheritedWidget {
+  final bool debugHighlightObserverRebuild;
+
+  const DebugRender({
+    super.key,
+    this.debugHighlightObserverRebuild = false,
+    required super.child,
+  });
+
+  static DebugRender? of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<DebugRender>();
+  }
+
+  @override
+  bool updateShouldNotify(covariant DebugRender oldWidget) {
+    return debugHighlightObserverRebuild != oldWidget.debugHighlightObserverRebuild;
   }
 }
