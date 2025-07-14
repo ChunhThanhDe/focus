7c1a6d111600d56da67d258ea684a1159ba81e99
ChunhThanhDe <chunhthanhde.dev@gmail.com>
2025-07-14 10:16:12 +0700

    chore: update dependencies and improve color generator scripts

--- scripts/generators/gradients_generator.dart
+++ scripts/generators/gradients_generator.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:dart_style/dart_style.dart';
 import 'package:characters/characters.dart';
+import 'package:pub_semver/pub_semver.dart';
 
 /// Run command: dart color_generator.dart <input.json> <output.dart>
 /// Full command to run from project root:
@@ -58,7 +59,9 @@ void main(List<String> args) {
   stringBuffer.writeln('}');
 
   // Format
-  final String output = DartFormatter().format(stringBuffer.toString());
+  final String output = DartFormatter(
+    languageVersion: Version.parse('3.7.0'),
+  ).format(stringBuffer.toString());
 
   // print to console
   stdout.writeln(output);
