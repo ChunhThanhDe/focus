f366c346ab4d962ed4800c3f15ed186315fdf82a
Chung Nguyen Thanh <chunhthanhde.dev@gmail.com>
2025-11-23 14:19:26 +0700

    chore: bump version to 2.7.1

--- extension/newtab/flutter_bootstrap.js
+++ extension/newtab/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4064631174"
+    serviceWorkerVersion: "3244172284"
   }
 });
